e95c2df57ef83520487e9071d80a93dd5c69117f
Ripretention <r5r44e@gmail.com>
2022-10-23 17:54:22 +0300

    Add main Tg class

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
-export { Api } from "./src/Api";
-export { Polling } from "./src/Polling";
-export { UpdateHandler } from "./src/UpdateHandler";
+import {Api} from "./src/Api";
+import {Polling} from "./src/Polling";
+import {UpdateHandler} from "./src/UpdateHandler";
+
+export class Tg {
+	constructor(private readonly token: string) {}
+	public readonly api = new Api(this.token);
+	public readonly updates = new UpdateHandler(this.api);
+	public readonly polling = new Polling(this.api);
+}
+export { Api as TgApi, Polling as TgPolling, UpdateHandler as TgUpdateHandler };
