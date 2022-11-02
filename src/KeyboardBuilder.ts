0d7e6c78de8230de31726a347b58def9e3bcd738
Ripretention <r5r44e@gmail.com>
2022-11-02 20:26:14 +0300

    Refactor, fix typo, add convintient way to add button in keyboard

--- src/KeyboardBuilder.ts
+++ src/KeyboardBuilder.ts
@@ -8,17 +8,20 @@ export abstract class BaseKeyboardBuilder<TButton extends BaseButton> {
 	protected buttons: TButton[][] = [];
 
 	public add(btn: TButton) {
-		let lastRow = Math.min(this.buttons.length-1, 0);
-		if (!this.buttons[lastRow])
-			this.buttons[lastRow] = [];
-		this.buttons[lastRow].push(btn);
+		let lastRow = this.buttons.length-1;
+		if (!this.buttons[lastRow] || this.buttons[lastRow].length > 8) {
+			this.wrap();
+			lastRow++;
+		}
 
+		this.buttons[lastRow].push(btn);
 		return this;
 	}
 	public wrap() {
 		this.buttons.push([]);
 		return this;
 	}
+	public abstract createButton(...args: any[]): this;
 	public abstract build(): string;
 }
 
@@ -55,6 +58,9 @@ export class InlineButton extends BaseButton {
 	}
 }
 export class InlineKeyboardBuilder extends BaseKeyboardBuilder<InlineButton> {
+	public createButton(...args: ConstructorParameters<typeof InlineButton>) {
+		return this.add(new InlineButton(...args));
+	}
 	public build(): string {
 		return JSON.stringify({
 			inline_keyboard: this.buttons.map(row => row.map(btn => btn.toObject()))
@@ -88,7 +94,7 @@ export class Button extends BaseButton {
 		return btn;
 	}
 }
-export class KeyboardBuilderBuilder extends BaseKeyboardBuilder<Button> {
+export class KeyboardBuilder extends BaseKeyboardBuilder<Button> {
 	private additionalOptions: Partial<IKeyboardButton> = {};
 	public setResize(state = true) {
 		this.additionalOptions["resize_keyboard"] = state;
@@ -107,6 +113,9 @@ export class KeyboardBuilderBuilder extends BaseKeyboardBuilder<Button> {
 		return this;
 	}
 
+	public createButton(...args: ConstructorParameters<typeof Button>) {
+		return this.add(new Button(...args));
+	}
 	public build(): string {
 		return JSON.stringify({
 			keyboard: this.buttons.map(row => row.map(btn => btn.toObject())),
